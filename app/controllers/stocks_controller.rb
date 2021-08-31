f1468533c6a631d77a7d674a0395998b42b570b5
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-08-31 12:06:11 -0400

    Used javascript.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,14 +4,20 @@ class StocksController < ApplicationController
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
       if @stock
-        render 'users/my_portfolio'
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+        end
       else  
-        flash[:alert] = "Please enter a valid ticker symbol to search"
-        redirect_to my_portfolio_path
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a valid ticker symbol to search"
+          format.js { render partial: 'users/result' }
+        end
       end
     else   
-      flash[:alert] = "Please enter a ticker symbol to search"
-      redirect_to my_portfolio_path
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a ticker symbol to search"
+        format.js { render partial: 'users/result' }
+      end
     end
   end
 
