2043a47bff1189c810efa4d797948e13625b6306
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-09-03 11:44:52 -0400

    Ability to remove stocks from portfolio.

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -11,4 +11,13 @@ class UserStocksController < ApplicationController
     redirect_to my_portfolio_path
   end
 
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    user_stock.destroy
+    flash[:notice] = "#{stock.ticker} was successfully removed from your portfolio"
+    redirect_to my_portfolio_path
+  end
+
+
 end
